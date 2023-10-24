156fef79c6f616bb24efd11e3919b59c72d39f4e
Jonathan Bijl <sannoayo@gmail.com>
2023-10-24 09:31:37 -0400

    Added Greeting.js and Create class and function components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import React from 'react';
 import Navbar from './components/Navbar';
+import Greeting from './components/Greeting';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Greeting />
+      <hr />
+      <Greeting />
+      <hr />
+      <Greeting />
     </div>
   );
 }
